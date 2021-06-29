31d1278947e3c451118f19297a863c14e968110a
pepesan <pepesan@gmail.com>
2021-06-29 11:36:25 +0200

    inicial

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Calculator {
+    public Calculator(){
+
+    }
+    public int add(int a, int b) {
+        return a+b;
+    }
+}
---
+++ src/test/java/org/example/CalculatorTests.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTests {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void addition() {
+        assertEquals(2, calculator.add(1, 1));
+    }
+}
